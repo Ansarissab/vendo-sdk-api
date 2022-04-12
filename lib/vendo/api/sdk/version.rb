302d726c7ee03dfaacb2078451a388010a48d06d
Ansarissab <zahidensari116@gmail.com>
2022-04-12 19:19:13 +0500

    Initial commit

---
+++ lib/vendo/api/sdk/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Vendo
+  module Api
+    module Sdk
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ spec/vendo/api/sdk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Vendo::Api::Sdk do
+  it "has a version number" do
+    expect(Vendo::Api::Sdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
